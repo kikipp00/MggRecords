b8058458c173456d6385b59b04cb79b57942a9e8
kikipp00 <ttd200003@utdallas.edu>
2024-01-20 01:37:37 -0600

    remove csv, go through all categories

--- mgg.py
+++ mgg.py
@@ -1,7 +1,6 @@
 from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
 import time
-import csv
 from concurrent.futures import ThreadPoolExecutor
 import threading
 import sqlite3
@@ -12,6 +11,7 @@ from sqlite3 import Error
 # ssl._create_default_https_context = ssl._create_unverified_context
 
 lock = threading.Lock()
+database = "mangago.db"
 
 
 # create a database connection to a SQLite database
@@ -24,19 +24,12 @@ def create_connection(db_file):
     return conn
 
 
-def create_table(conn, create_table_sql):
+def create_table(conn, schema):
     try:
         c = conn.cursor()
-        c.execute(create_table_sql)
+        c.execute(schema)
     except Error as e:
         print(e)
-
-
-def add_row(conn, items):
-    sql = "INSERT INTO manga(name,link,author,alt_title) VALUES (?,?,?,?)"
-    cur = conn.cursor()
-    cur.execute(sql, items)
-    conn.commit()
 
 
 def bs_webpage(url, parser):
@@ -73,54 +66,70 @@ def scan_entry(entry):
     return title, url, authors, alt_titles
 
 
-def write_entry(writer, entry):
+def insert_entry(entry, category):
     title, url, authors, alt_titles = scan_entry(entry)
     with lock:
-        writer.writerow([title, url, authors, alt_titles])
-        conn = create_connection("mangago.db")
+        conn = create_connection(database)
         with conn:
-            row = (title, url, authors, alt_titles)
-            add_row(conn, row)
+            items = (title, url, authors, alt_titles)
+            cur = conn.cursor()
+            if category == 1:
+                cur.execute("INSERT INTO Want(name, link, author, alt_title) VALUES (?,?,?,?)", items)
+            elif category == 2:
+                cur.execute("INSERT INTO Reading(name, link, author, alt_title) VALUES (?,?,?,?)", items)
+            else:
+                cur.execute("INSERT INTO AlreadyRead(name, link, author, alt_title) VALUES (?,?,?,?)", items)
+            conn.commit()
 
 
 def main():
-    conn = create_connection("mangago.db")
+    start_time = time.time()
+    conn = create_connection(database)
     with conn:
-        table = """ CREATE TABLE IF NOT EXISTS manga (
+        table_want = """ CREATE TABLE IF NOT EXISTS Want(
                                                     name text NOT NULL PRIMARY KEY,
                                                     link text,
                                                     author text,
                                                     alt_title text
                                                 ); """
-        create_table(conn, table)
-
-    start_time = time.time()
-    init_run = True
-    max_page = 1
-    page_no = 1
-
-    with open('test.csv', 'w', newline='') as file:  # don't need to explicitly close bc "with"
-        writer = csv.writer(file)
-        writer.writerow(['TITLE', 'LINK', 'AUTHOR(S)', 'ALT-TITLE(S)'])
+        table_reading = """ CREATE TABLE IF NOT EXISTS Reading(
+                                                            name text NOT NULL PRIMARY KEY,
+                                                            link text,
+                                                            author text,
+                                                            alt_title text
+                                                        ); """
+        table_read = """ CREATE TABLE IF NOT EXISTS AlreadyRead(
+                                                                    name text NOT NULL PRIMARY KEY,
+                                                                    link text,
+                                                                    author text,
+                                                                    alt_title text
+                                                                ); """
+        create_table(conn, table_want)
+        create_table(conn, table_reading)
+        create_table(conn, table_read)
 
         # traverse all pages
-        while page_no <= max_page:
-            url = f"https://www.mangago.me/home/people/29556/manga/1/?page={page_no}"  # todo: 1 = want, 2 = reading, 3 = read
-            page_no += 1
-            soup = bs_webpage(url, 'lxml')
+        for category in range(1, 4):
+            init_run = True
+            max_page = 1
+            page_no = 1
+            while page_no <= max_page:
+                url = f"https://www.mangago.me/home/people/3306689/manga/{category}/?page={page_no}"
+                page_no += 1
+                soup = bs_webpage(url, 'lxml')
 
-            # find max # of pages
-            if init_run:
-                init_run = False
-                option = soup.select('li > span > select > option')
-                if len(option) > 0:
-                    max_page = int(option[len(option) - 1].text.strip())
+                # find max # of pages
+                if init_run:
+                    init_run = False
+                    option = soup.select('li > span > select > option')
+                    if len(option) > 0:
+                        max_page = int(option[len(option) - 1].text.strip())
 
-            # traverse every manga in list
-            with ThreadPoolExecutor(max_workers=10) as executor:
-                threads = []
-                for entry in soup.findAll("h3", attrs={'class': 'title'}):
-                    threads.append(executor.submit(write_entry, writer, entry))
+                # traverse every manga in list
+                with ThreadPoolExecutor(max_workers=10) as executor:
+                    threads = []
+                    for entry in soup.findAll("h3", attrs={'class': 'title'}):
+                        threads.append(executor.submit(insert_entry, entry, category))
 
     end_time = time.time()
     elapsed_time = end_time - start_time
