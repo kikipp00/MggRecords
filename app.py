06748b8a7d99d84c3f181d05d21f4520c935affa
kikipp00 <ttd200003@utdallas.edu>
2024-01-25 23:37:05 -0600

    separate loading function, /result fails on refresh

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, send_from_directory
+from flask import Flask, render_template, request, send_from_directory, redirect, url_for
 import sqlite3
 import os
 import mgg
@@ -17,27 +17,30 @@ def form():
     return render_template('form.html')
 
 
-@app.route('/data', methods=['POST', 'GET'])
+@app.route('/', methods=['POST'])
 def data():
+    form_data = request.form
+    mgg.init_db()
+
+    # todo: loading page
+    if 'want' in request.form:
+        mgg.scan_category(1, form_data['userid'])
+    if 'reading' in request.form:
+        mgg.scan_category(2, form_data['userid'])
+    if 'read' in request.form:
+        mgg.scan_category(3, form_data['userid'])
+    return redirect(url_for('result'), code=307)
+
+
+@app.route('/result', methods=['POST', 'GET'])
+def result():
     if request.method == 'GET':
-        return f"The URL /data is accessed directly. Try going to '/form' to submit form"
+        return f"The URL /result is accessed directly. Try going to '/result' to submit form"
     if request.method == 'POST':
-        form_data = request.form
-        mgg.init_db()
-
-        # todo: loading page
-        if 'want' in request.form:
-            mgg.scan_category(1, form_data['userid'])
-        if 'reading' in request.form:
-            mgg.scan_category(2, form_data['userid'])
-        if 'read' in request.form:
-            mgg.scan_category(3, form_data['userid'])
-
         conn = mgg.create_connection(mgg.database)
         with conn:
             rows = conn.execute('SELECT * FROM Reading').fetchall()
             column_names = [fields[1] for fields in conn.execute("PRAGMA table_info(Reading)").fetchall()]
-
         return render_template('result.html', rows=rows, column_names=column_names)
 
 
